9d701efd298e6a61fd369a13cb78cd6126bb2060
Mohit Kumar <mr.mohit9582@gmail.com>
2025-07-17 17:40:08 +0530

    just remove the port which is not necessary because we used env files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const  {PORT} = require('./config/serverconfig');
 const setupAndStartServer = async () => {
   // create the express object
   const app = express();
-//   const PORT = 3000;
+
 
   app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
