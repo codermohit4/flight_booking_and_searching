836c776f9004f7ba7657e02aa5153617aa2d9ad3
Mohit Kumar <mr.mohit9582@gmail.com>
2025-07-23 00:46:44 +0530

    completed city repository

--- src/respository/city-repository.js
+++ src/respository/city-repository.js
@@ -1,11 +1,15 @@
 const { City } = require('../models/index');
 
+
+
+
 class CityRepository {
   async createCity({ name }) {
     try {
       const city = await City.create({ name });
       return city;
     } catch (error) {
+      console.log("Something went wrong in the repository layer");
       throw { error };
     }
   }
@@ -13,11 +17,32 @@ class CityRepository {
   async deleteCity(cityId) {
     try {
       await City.destroy({
-        where: {
-          id: cityId
-        }
+        where: { id: cityId }
       });
     } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: { id: cityId }
+      });
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
       throw { error };
     }
   }
