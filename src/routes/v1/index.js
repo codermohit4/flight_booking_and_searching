e87cb00953168c51e1bae1a7c14a3da000dc9b1f
Mohit Kumar <mr.mohit9582@gmail.com>
2025-07-24 23:35:08 +0530

    get , update  everything resolve

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,4 +2,7 @@ const express = require("express");
 const cityController = require("../../controllers/city-controller");
 const router = express.Router();
 router.post("/city", cityController.create);
+router.delete("/city/:id", cityController.destroy);
+router.get("/city/:id", cityController.get);
+
 module.exports = router;
